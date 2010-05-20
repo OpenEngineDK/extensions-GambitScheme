81e699add7f7f898c33277d0a87b7ea72001ba0d
ptx <ptx@openengine.dk>
2010-05-20 09:52:37 +0000

    Small cmakechanges

--- Scripting/init.scm
+++ Scripting/init.scm
@@ -1,7 +1,8 @@
 
 (define (hest)
-  (print "Hello World from gambit!\n")
-  )
+  (print "Hello World from gambit!\n"))
+
+(c-define-type Event "Event")
 
 (c-define (oe-init) () void "oe_init" ""
           (hest))
